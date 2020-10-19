b4139dae02cb77f8042385c528771b0ef09e2e4d
Eugene Strelkov <felixpochta@gmail.com>
2020-10-19 12:31:29 +0300

    add python enum checks  to travis ci

--- wrappers/nodejs/scripts/enums.py
+++ wrappers/nodejs/scripts/enums.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import re
 import os
 import sys
+import argparse
 
-ENUM_H_NAME_REGEXP = r'typedef enum (\w+)'
-ENUM_H_ITEM_REGEXP = r'^[ ]*(RS2_\w+)'
 
+ENUM_H_REGEXP = r'^[ ]*(RS2_\w+)'
 ENUM_CPP_REGEXP = r'^[ ]*_FORCE_SET_ENUM\((\w+)\)'
 ENUM_JS_REGEXP = r'RS2\.(RS2_\w+)'
 
@@ -21,26 +22,6 @@ def get_first_by_regexp(line, regexp):
     if res:
         return res[0]
     return ''
-
-
-def get_enums_from_h_file(file_path):
-    enums = []
-    with open(file_path) as f:
-        enum_name = ''
-        for line in f:
-            if not enum_name:
-                enum_name = get_first_by_regexp(line, ENUM_H_NAME_REGEXP)
-                continue
-
-            enum = get_first_by_regexp(line, ENUM_H_ITEM_REGEXP)
-
-            if enum:
-                enums.append(enum)
-
-            if bool(re.search(enum_name, line)):
-                enum_name = ''
-
-    return enums
 
 
 def get_enums_from_folder(folder_path, ends, regexp):
@@ -62,11 +43,11 @@ def get_enums_from_file(file_path, regexp):
     return enums
 
 
-def run(include_folder_path, addon_folder_path, js_file_path):
+def run(include_folder_path, addon_folder_path):
 
-    include_enums = get_enums_from_folder(include_folder_path, '.h', ENUM_H_ITEM_REGEXP)
+    include_enums = get_enums_from_folder(include_folder_path, '.h', ENUM_H_REGEXP)
     cpp_enums = get_enums_from_folder(addon_folder_path, '.cpp', ENUM_CPP_REGEXP)
-    js_enums = get_enums_from_file(js_file_path, ENUM_JS_REGEXP)
+    # js_enums = get_enums_from_file(js_file_path, ENUM_JS_REGEXP)
 
     include_enums.sort()
     cpp_enums.sort()
@@ -77,10 +58,16 @@ def run(include_folder_path, addon_folder_path, js_file_path):
 
 
 if __name__ == '__main__':
-    (include_folder_path, addon_folder_path, js_file_path) = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Ping script')
+    parser.add_argument('-i', action='store', dest='include_folder_path', required=True)
+    parser.add_argument('-a', action='store', dest='addon_folder_path', required=True)
+    parser.add_argument('-v', '--verbose', action='store_true', help='print not user enums')
 
-    missed = run(include_folder_path, addon_folder_path, js_file_path)
+    args = parser.parse_args()
+
+    missed = run(args.include_folder_path, args.addon_folder_path)
+
     if missed:
-        sys.exit(missed)
+        sys.exit(missed if args.verbose else 1)
 
     sys.exit()
